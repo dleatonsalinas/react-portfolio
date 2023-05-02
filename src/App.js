05788a9605200c3f4a5f95fbebb281b3868ddcdc
dleatonsalinas <daniellesalinas721@gmail.com>
2023-05-02 13:17:01 -0500

    update file structure

--- src/App.js
+++ src/App.js
@@ -1,18 +1,6 @@
 import React from "react";
-import Header from "./components/Header";
-import Navigation from "./components/Navigation";
-import Main from "./components/Main"
-import Footer from "./components/Footer";
+import PortfolioContainer from "./components/PortfolioContainer";
 
-function App() {
-  return (
-    <div>
-      <Navigation />
-      <Header />
-      <Main />
-      <Footer />
-    </div>
-  );
-}
+const App = () => <PortfolioContainer />;
 
 export default App;
