1ac820ea912915503d60cc3429cc003b2746468d
dleatonsalinas <daniellesalinas721@gmail.com>
2023-05-03 08:32:26 -0500

    Update Contact section

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,22 +1,26 @@
 import React from 'react';
+import '../styles/style.css';
 
 export default function Contact() {
   return (
-    <div>
-      <h1>Contact Page</h1>
-      <p>
-        Integer cursus bibendum sem non pretium. Vestibulum in aliquet sem, quis
-        molestie urna. Aliquam semper ultrices varius. Aliquam faucibus sit amet
-        magna a ultrices. Aenean pellentesque placerat lacus imperdiet
-        efficitur. In felis nisl, luctus non ante euismod, tincidunt bibendum
-        mi. In a molestie nisl, eu sodales diam. Nam tincidunt lacus quis magna
-        posuere, eget tristique dui dapibus. Maecenas fermentum elementum
-        faucibus. Quisque nec metus vestibulum, egestas massa eu, sollicitudin
-        ipsum. Nulla facilisi. Sed ut erat ligula. Nam tincidunt nunc in nibh
-        dictum ullamcorper. Class aptent taciti sociosqu ad litora torquent per
-        conubia nostra, per inceptos himenaeos. Etiam ornare rutrum felis at
-        rhoncus. Etiam vel condimentum magna, quis tempor nulla.
-      </p>
+    <div className="contact-bkrd">
+      <section className="contact" id="contact">
+        <h5>CONTACT</h5>
+        <div className="contact-container">
+          <form action="mailto: daniellesalinas721@gmail.com">
+            <label htmlFor="yname">Your Name</label>
+            <input type="text" id="yname" name="yourname" placeholder="Your Name" />
+
+            <label htmlFor="email">E-mail Address</label>
+            <input type="text" id="email" name="email" placeholder="E-mail Address" />
+
+            <label htmlFor="subject">Subject</label>
+            <textarea id="subject" name="subject" placeholder="Say hello!" style={{height: "200px"}}></textarea>
+
+            <input className="submit" type="submit" value="Submit" />
+          </form>
+        </div>
+      </section>
     </div>
   );
 }
