23c1b5329c385e452a780b580af9b03295ce2914
dleatonsalinas <daniellesalinas721@gmail.com>
2023-06-01 16:05:05 -0500

    Begin working on LitChat info page, under work description

---
+++ src/components/pages/LitChat.js
@@ -0,0 +1,126 @@
+import React from 'react';
+import '../styles/style.css';
+// import DLSImg from '../images/DLSHeadshot2.jpg';
+
+export default function LitChat() {
+  return (
+    <div className="project" id="litchat">
+      <div className="container bg-white">
+        <h1>LitChat</h1>
+        <p>A webiste where users can discuss and exchange ideas about literature</p>
+        {/* mobile image and desktop image */}
+      </div>
+      <div className="container bg-grey">
+        <h2>Project Overview</h2>
+        {/* COLUMN LEFT */}
+        <h3>Challenge</h3>
+        <p>LitChat LitChat is a dynamic and engaging website designed for literature enthusiasts to connect and share their thoughts on their favorite books. It serves as a virtual gathering place where users from all over can come together to discuss and exchange ideas about literature.</p>
+
+        <h3>Objectives</h3>
+        <p>
+          <ul>
+            <li>Design the user interface for Litchat that is user-friendly and accessible from a desktop or mobile device</li>
+            <li>Design a logo for LitChat to use on the website and branding material</li>
+          </ul>
+        </p>
+
+        {/* COLUMN RIGHT */}
+        <h4>Project Scope</h4>
+        <p>Front-End Web Development, User Interface, and Branding</p>
+
+        <h4>Tools</h4>
+        <p>Figma, HTML5, CSS3, JavaScript, React</p>
+
+        <h4>Role</h4>
+        <p>Sole UX / UI Designer and Developer and Front-End Web Developer</p>
+
+        <h4>Team</h4>
+        <p>Danielle Leaton-Salinas</p> https://github.com/dleatonsalinas
+        <p>Alan Hornbaker</p> https://github.com/alanhornbaker
+        <p>Houston Asher Laws</p> https://github.com/hasherlaws18
+        <p>Matin Tavacoli</p> https://github.com/mtavacoli
+
+        <h4>Duration</h4>
+        <p> 2 Weeks (15 Class Hours, 15 Independent Hours)</p>
+
+        {/* BUTTON TO VIEW PROTOTYPE / GITHUB LAUNCH */}
+      </div>
+      <div>
+        <h2>Design Process</h2>
+        <section>
+          <h3>Research</h3>
+          <ul>
+            <li>User Research</li>
+            <li>Competitor Analysis</li>
+            <li>Gather Insights</li>
+            <li>Market Research</li>
+            <li>Define Project Goals</li>
+            <li>User Personas</li>
+          </ul>
+        </section>
+        <section>
+          <h3>Design</h3>
+          <ul>
+            <li>Wireframes</li>
+            <li>Visual Mockups</li>
+            <li>Task Flow</li>
+            <li>Branding</li>
+            <li>Responsive UI</li>
+          </ul>
+        </section>
+        <section>
+          <h3>Prototyping</h3>
+          <ul>
+            <li>Mockups</li>
+            <li>User Flows</li>
+            <li>Realistic Content</li>
+            <li>Feedback</li>
+            <li>Internal Testing</li>
+            <li>Review</li>
+          </ul>
+        </section>
+        <section>
+          <h3>Testing</h3>
+          <ul>
+            <li>Usability Testing</li>
+            <li>Defined Tasks</li>
+            <li>Collect Data</li>
+            <li>Improve User Experience</li>
+          </ul>
+        </section>
+        <section>
+          <h2>Research</h2>
+          <h3>Research Plan</h3>
+          <p>Researching gives me a better understanding of the target users, their behaviors, needs, motivations, and prefereces. By researching first, I am able to uncover users' challenges and frustrations to create a more empathetic and user-friendly solution.
+
+            Creating a research plan is crucial to provide effective guidance for responsive design and development. The research plan for LitChat includes a project background, research questions, assumptions, methodologies, participants, and timeline. A complete research plan can be found here.   </p>
+          {/* TODO ADD RESEARCH PLAN LINK */}
+
+          <h4>Rsearch Goals</h4>
+          <ul>
+            <li>Evaluate the existing market landscape and identify potential differentiators for LitChat.</li>
+            <li>Gain a deep understanding of the target audience's demographics, preferences, motivations, and behaviors related to book reading and online communities.</li>
+            <li>Conduct a competitive analysis to assess competitors' strengths and weaknesses and identify opportunities for differentiation.</li>
+            <li>Investigate how users currently manage their book lists and explore their preferences for organizing and tracking books.</li>
+            <li>Explore users' experiences in finding books they want to read, including discovery methods, sources of recommendations, and challenges they face.</li>
+          </ul>
+          <h4>Assumptions</h4>
+          <ul>
+            <li>LitChat’s primary user base consists of avid book lovers</li>
+            <li>People will actively discuss books with other people they do not know</li>
+            <li>People want to find books to read online</li>
+          </ul>
+          <h4>Methodologies</h4>
+          <ul>
+            <li>Secondary Research: Market Research, Competitive Analysis</li>
+            <li>Primary Research: User Interviews</li>
+          </ul>
+          <h3>Secondary Research</h3>
+          <h4>Market Research</h4>
+          <p>Market research is crucial for design as it helps to understand the competitive landscape, user expectations, and market trends. It provides valuable insignts into user needs, preferences, and behaviors. Researching allows me create user-centered solutions by identifying gaps, opportunities, and potential differentiators. This ensures that the designs are not only aesthetically pleasing but also aligned with business objectives and capable of delivering a superior user experience.</p>
+        <h5>Book Industry & Platforms</h5>
+        </section>
+      </div>
+    </div>
+  );
+}
